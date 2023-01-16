6794b49b997bf95023b787aa41a8350fd5637284
Polandian <101973563+Polandian@users.noreply.github.com>
2023-01-16 03:59:19 +0300

    Update MyScripts.lua

--- MyScripts.lua
+++ MyScripts.lua
@@ -7,81 +7,81 @@ if introSaid == false then
 	introSaid = true
 end
 
+game:GetService("UserInputService").InputBegan:connect(function(inputObject, gameprocess)
+    if (not gameprocess) then
+         u.InputBegan:Connect(function(input) 
 
-u.InputBegan:Connect(function(input) 
+    		-- TELEPORT TO A RANDOM PLAYER IN SERVER
+    		if input.KeyCode == Enum.KeyCode.F then
+        		local Players = game.Players:GetPlayers()
+			local nplrs = #Players
+			local Randomplayer1 = nil
+			local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+			local humanoid = game.Players.LocalPlayer.Character.Humanoid
+			if nplrs > 0 then
+    				Randomplayer1 = Players[math.random(1, nplrs)]
+			end
 
-    -- TELEPORT TO A RANDOM PLAYER IN SERVER
-    if input.KeyCode == Enum.KeyCode.F then
-        local Players = game.Players:GetPlayers()
-		local nplrs = #Players
-		local Randomplayer1 = nil
-		local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-		local LocalPlayer = game. Players.LocalPlayer
-		local humanoid = game.Players.LocalPlayer.Character.Humanoid
-		local players = game.Players:GetChildren()
-		if nplrs > 0 then
-    			local  RandomPlayer = player [math.random(1, #Player)]
-			repeat  RandomPlayer = player [math.random(1, #Player)] until RandomPlayer ~=  LocalPlayer
-		end
+			local Randomplayer = tostring(Randomplayer1)
 
-		local Randomplayer = tostring(Randomplayer1)
+			print(Randomplayer)
 
-		print(Randomplayer)
-
-		humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-		wait(0.1)
-
-		pl.CFrame = game.Players[Randomplayer].Character.HumanoidRootPart.CFrame
-    end
-
-    -- INFINITE JUMP
-    if input.KeyCode == Enum.KeyCode.Space then
-		local humanoid = game.Players.LocalPlayer.Character.Humanoid
-		humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-		wait(0.1)
-	end
-
-	-- FIND LOCATION
-	if input.KeyCode == Enum.KeyCode.G then
-		local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-		print(pl.CFrame)
-	end
-
-    -- TELEPORT TO SPESIFIC LOCATION
-	-- if input.KeyCode == Enum.KeyCode.H then
-		-- local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-		-- pl.CFrame = CFrame.new(413, 45, 4576)
-	-- end
-
-    -- STOPS BANGING SESSION
-    if input.KeyCode == Enum.KeyCode.M then
-	teleportingEnabled = false
-	warn("You stopped teleporting!")
-    end
-
-	-- PICKS RANDOM PLAYER TO START BANGING SECTION
-    if input.KeyCode == Enum.KeyCode.N then
-
-        local Players = game.Players:GetPlayers()
-		local nplrs = #Players
-		local Randomplayer1 = nil
-		local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-		local humanoid = game.Players.LocalPlayer.Character.Humanoid
-		if nplrs > 0 then
-    		Randomplayer1 = Players[math.random(1, nplrs)]
-		end
-
-		local Randomplayer = tostring(Randomplayer1)
-
-		print(Randomplayer)
-
-		teleportingEnabled = true
-		
-		while teleportingEnabled == true do
 			humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
 			wait(0.1)
-			
+
 			pl.CFrame = game.Players[Randomplayer].Character.HumanoidRootPart.CFrame
-		end	
-	end
+   		end
+
+    		-- INFINITE JUMP
+    		if input.KeyCode == Enum.KeyCode.Space then
+			local humanoid = game.Players.LocalPlayer.Character.Humanoid
+			humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+			wait(0.1)
+		end
+
+		-- FIND LOCATION
+		if input.KeyCode == Enum.KeyCode.G then
+			local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+			print(pl.CFrame)
+		end
+
+    		-- TELEPORT TO SPESIFIC LOCATION
+			-- if input.KeyCode == Enum.KeyCode.H then
+				-- local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+				-- pl.CFrame = CFrame.new(413, 45, 4576)
+			-- end
+
+    		-- STOPS BANGING SESSION
+   	 	if input.KeyCode == Enum.KeyCode.M then
+			teleportingEnabled = false
+			warn("You stopped teleporting!")
+    		end
+
+		-- PICKS RANDOM PLAYER TO START BANGING SECTION
+   		if input.KeyCode == Enum.KeyCode.N then
+
+       			local Players = game.Players:GetPlayers()
+			local nplrs = #Players
+			local Randomplayer1 = nil
+			local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+			local humanoid = game.Players.LocalPlayer.Character.Humanoid
+			if nplrs > 0 then
+    			Randomplayer1 = Players[math.random(1, nplrs)]
+			end
+
+			local Randomplayer = tostring(Randomplayer1)
+
+			print(Randomplayer)
+
+			teleportingEnabled = true
+		
+			while teleportingEnabled == true do
+				humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+				wait(0.1)
+			
+				pl.CFrame = game.Players[Randomplayer].Character.HumanoidRootPart.CFrame
+			end	
+		end
+	end)
+    end
 end)
